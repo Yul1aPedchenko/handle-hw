8a7ba5513f52b3692ff66fb1d0a81355fd02689f
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-03-09 13:48:51 +0200

    last

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
-// import NewApiService from "./js/js-search";
-// import Handlebars from "handlebars";
-
-class NewApiService {
-  constructor() {
-    (this.type = true), (this.page = 1);
-  }
-  async fetchImages() {
-    const API_KEY = "49248901-d6c95cf50118914c4de622d87";
-    const url = `https://pixabay.com/api/?key=${API_KEY}&editors_choice=${this.type}&page=${this.page}&per_page=9`;
-    const response = await fetch(url);
-    const data = await response.json();
-    this.incrementPage();
-    return data.hits || [];
-  }
-  incrementPage() {
-    this.page += 1;
-  }
-}
-
-
-
+import NewApiService from "./js/js-search";
+import Handlebars from "handlebars";
 const refs = {
   btnLoadMore: document.getElementById("load-more-btn"),
   galleryBody: document.getElementById("image-gallery"),
@@ -33,16 +13,16 @@ function onLoadMore() {
   newApiService.fetchImages().then(appendImagesMarkup);
 }
 function appendImagesMarkup(images) {
-  // const templateSource = `
-  //   {{#each this}}
-  //       <img src="{{webformatURL}}" alt="tags">
-  //   {{/each}}
-  //   `;
-  // console.log(images);
-  // const template = Handlebars.compile(templateSource);
-  // const markup = template(images);
-  const markUp = images.map(image => {
-    return `<img src="${image.webformatURL}" alt="${image.tags}">`;
-  })
-  refs.galleryBody.insertAdjacentHTML("beforeend", markUp.join(''));
+  const templateSource = `
+    {{#each this}}
+        <img src="{{webformatURL}}" alt="tags">
+    {{/each}}
+    `;
+  console.log(images);
+  const template = Handlebars.compile(templateSource);
+  const markUp = template(images);
+  // const markUp = images.map(image => {
+  //   return `<img src="${image.webformatURL}" alt="${image.tags}">`;
+  // })
+  refs.galleryBody.insertAdjacentHTML("beforeend", markUp);
 }
