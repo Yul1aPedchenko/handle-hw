4edd7273a7874ff1d8dc9d85fdd19a0bec5afa99
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-03-09 12:36:14 +0200

    hw-pagination

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import NewApiService from "./js/js-search";
+const refs = {
+  btnLoadMore: document.getElementById("load-more-btn"),
+  galleryBody: document.getElementById("image-gallery"),
+};
+const newApiService = new NewApiService();
+document.addEventListener('DOMContentLoaded', () => {
+    newApiService.fetchImages().then(appendImagesMarkup);
+})
+refs.btnLoadMore.addEventListener("click", onLoadMore);
+function onLoadMore() {
+  newApiService.fetchImages().then(appendImagesMarkup);
+}
+function appendImagesMarkup(images) {
+  const templateSource = `
+    {{#each this}}
+        <img src="{{webformatURL}}" alt="tags">
+    {{/each}}
+    `;
+  console.log(images);
+  const template = Handlebars.compile(templateSource);
+  const markup = template(images);
+  refs.galleryBody.insertAdjacentHTML("beforeend", markup);
+}
