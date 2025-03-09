c754f187333af157db151fbfb2a743b90fc7b7d4
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-03-09 13:18:47 +0200

    try without modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import NewApiService from "./js/js-search";
-import Handlebars from "handlebars";
+// import NewApiService from "./js/js-search";
+// import Handlebars from "handlebars";
+
+class NewApiService {
+  constructor() {
+    (this.type = true), (this.page = 1);
+  }
+  async fetchImages() {
+    const API_KEY = "49248901-d6c95cf50118914c4de622d87";
+    const url = `https://pixabay.com/api/?key=${API_KEY}&editors_choice=${this.type}&page=${this.page}&per_page=9`;
+    const response = await fetch(url);
+    const data = await response.json();
+    this.incrementPage();
+    return data.hits || [];
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+}
+
+
+
 const refs = {
   btnLoadMore: document.getElementById("load-more-btn"),
   galleryBody: document.getElementById("image-gallery"),
