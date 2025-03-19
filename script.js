f6e0d67791daac3fe8247e3a5351e3f835f6a111
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-03-19 21:56:45 +0200

    update hw-19 (add hw-18)

--- script.js
+++ script.js
@@ -53,6 +53,12 @@ refs.tableBody.addEventListener("click", (e) => {
     deleteStudent(studentId);
   }
 });
+// function getStudents() {
+//   return fetch(`${BASE_URL}`)
+//     .then((r) => r.json())
+//     .then((students) => renderStudents(students))
+//     .catch((error) => console.log(`Some problems with get students: ${error}`));
+// }
 async function getStudents() {
   try {
     const response = await fetch(`${BASE_URL}`);
@@ -81,6 +87,33 @@ function renderStudents(students) {
 
   refs.tableBody.insertAdjacentHTML("beforeend", markUp.join(" "));
 }
+// function addStudent(e) {
+//   e.preventDefault();
+//   const name = document.getElementById("name").value;
+//   const age = document.getElementById("age").value;
+//   const course = document.getElementById("course").value;
+//   const skills = document
+//     .getElementById("skills")
+//     .value.split(",")
+//     .map((skill) => skill.trim())
+//     .filter((skill) => skill);
+//   const email = document.getElementById("email").value;
+//   const isEnrolled = document.getElementById("isEnrolled").checked;
+//   const newStudent = {
+//     name,
+//     age,
+//     course,
+//     skills,
+//     email,
+//     isEnrolled,
+//   };
+//   const newOptions = new Options("POST", JSON.stringify(newStudent));
+//   fetch(`${BASE_URL}`, newOptions)
+//     .then((r) => r.json())
+//     .then(() => getStudents())
+//     .catch((error) => console.error(`Error with adding student: ${error}`));
+//   refs.form.reset();
+// }
 async function addStudent(e) {
   e.preventDefault();
   const name = document.getElementById("name").value;
@@ -110,6 +143,22 @@ async function addStudent(e) {
   }
   refs.form.reset();
 }
+// function openModal(id) {
+//   fetch(`${BASE_URL}/${id}`)
+//     .then((r) => r.json())
+//     .then((student) => {
+//       document.getElementById("edit-name").value = student.name;
+//       document.getElementById("edit-age").value = student.age;
+//       document.getElementById("edit-course").value = student.course;
+//       document.getElementById("edit-skills").value = student.skills.join(", ");
+//       document.getElementById("edit-email").value = student.email;
+//       document.getElementById("edit-isEnrolled").checked = student.isEnrolled;
+//       refs.modal.modalForm.setAttribute("data-id", student.id);
+//     })
+//     .catch((error) =>
+//       console.log(`Some problems with get student for edit-modal: ${error}`)
+//     );
+// }
 async function openModal(id) {
   try {
     const response = await fetch(`${BASE_URL}/${id}`);
@@ -129,6 +178,13 @@ async function openModal(id) {
   }
   refs.modal.modalBody.style.display = "block";
 }
+// function updateStudent(id, updateInfo) {
+//   const newOptions = new Options("PUT", JSON.stringify(updateInfo));
+//   fetch(`${BASE_URL}/${id}`, newOptions)
+//     .then((r) => r.json())
+//     .then(() => getStudents())
+//     .catch((error) => console.log(`Error with update student info: ${error}`));
+// }
 async function updateStudent(id, updateInfo) {
   try {
     const newOptions = new Options("PUT", JSON.stringify(updateInfo));
@@ -138,6 +194,14 @@ async function updateStudent(id, updateInfo) {
     console.error(`Error with update student info: ${error}`);
   }
 }
+
+// function deleteStudent(id) {
+//   const newOptions = new Options("DELETE");
+//   fetch(`${BASE_URL}/${id}`, newOptions)
+//     .then((r) => r.json())
+//     .then(() => getStudents())
+//     .catch((error) => console.log(`Error with delete student: ${error}`));
+// }
 async function deleteStudent(id) {
   try {
     const newOptions = new Options("DELETE");
