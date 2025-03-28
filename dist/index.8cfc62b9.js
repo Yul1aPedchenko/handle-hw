dc9a7a8a7cc07334286c1aee324cd34b9ae4defa
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-03-28 21:13:59 +0200

    finish update function

--- dist/index.8cfc62b9.js
+++ dist/index.8cfc62b9.js
@@ -626,27 +626,44 @@ async function createPost(title, content) {
     }
 }
 // Оновлення поста
-async function updatePost(id, title, content) {}
+async function updatePost(id, title, content) {
+    try {
+        const newBody = JSON.stringify(new (0, _post.basePost)(title, content));
+        const options = {
+            method: "PATCH",
+            body: newBody,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+        const r = await fetch(`${BASE_URL}/${id}`, options);
+        const res = await r.json();
+        console.log(res);
+        startApp();
+    } catch (error) {
+        console.error(error);
+    }
+}
 // Видалення поста
 async function deletePost(id) {}
 // Додавання коментаря до поста
 async function createComment(postId, comment) {}
 // // Оновлення відображення постів на сторінці
 function renderPosts(posts) {
-    document.getElementById("postsContainer").innerHTML = '';
+    document.getElementById("postsContainer").innerHTML = "";
     const markUp = posts.map((post)=>{
         const comments = post.comments;
         return `
             <div class="post">
                 <h2>${post.title}</h2>
-                <p>${post.text}</p>
+                <p>${post.content}</p>
                 <button class="editPostButton" data-id="${post.id}">\u{420}\u{435}\u{434}\u{430}\u{433}\u{443}\u{432}\u{430}\u{442}\u{438}</button>
                 <button class="deletePostButton" data-id="${post.id}">\u{412}\u{438}\u{434}\u{430}\u{43B}\u{438}\u{442}\u{438}</button>
                 <div class="commentsContainer" data-id="${post.id}">
                     <h3>\u{41A}\u{43E}\u{43C}\u{435}\u{43D}\u{442}\u{430}\u{440}\u{456}:</h3>
                     <ul>
                     ${comments.map((comment)=>{
-            return `<li>${comment.content}</li>`;
+            return `<li>${comment.text}</li>`;
         }).join("")}
                     </ul>
                     <form class="createCommentForm">
@@ -659,6 +676,13 @@ function renderPosts(posts) {
                     <button type="submit">\u{414}\u{43E}\u{434}\u{430}\u{442}\u{438} \u{43A}\u{43E}\u{43C}\u{435}\u{43D}\u{442}\u{430}\u{440}</button>
                     </form>
                 </div>
+              <div class="form-wrap">
+              <form>
+                <input type="text" id="editTitleInput" placeholder="\u{417}\u{430}\u{433}\u{43E}\u{43B}\u{43E}\u{432}\u{43E}\u{43A}" required />
+                <textarea id="editContentInput" placeholder="\u{417}\u{43C}\u{456}\u{441}\u{442}" required></textarea>
+                <button type="submit" id="editBtn">\u{417}\u{431}\u{435}\u{440}\u{435}\u{433}\u{442}\u{438} \u{437}\u{43C}\u{456}\u{43D}\u{438}</button>
+              </form>
+    </div>
             </div>
             `;
     });
@@ -670,9 +694,32 @@ document.getElementById("createPostForm").addEventListener("submit", (e)=>{
     const title = document.getElementById("titleInput").value;
     const content = document.getElementById("contentInput").value;
     createPost(title, content);
+    document.getElementById("createPostForm").reset();
 });
 // // Обробник події для редагування поста
-// document.addEventListener("click", cb);
+document.getElementById("postsContainer").addEventListener("click", (e)=>{
+    let curId = null;
+    if (e.target.classList.contains("editPostButton")) {
+        e.preventDefault();
+        curId = e.target.dataset.id;
+        const postEl = e.target.closest(".post");
+        const formEl = postEl.querySelector(".form-wrap");
+        if (formEl) {
+            formEl.style.display = "block";
+            formEl.querySelector('#editTitleInput').value = postEl.querySelector('h2').textContent;
+            formEl.querySelector("#editContentInput").value = postEl.querySelector("p").textContent;
+        }
+        const editBtn = postEl.querySelector('#editBtn');
+        if (editBtn) editBtn.addEventListener('click', async (event)=>{
+            event.preventDefault();
+            const title = postEl.querySelector('#editTitleInput').value;
+            const content = postEl.querySelector('#editContentInput').value;
+            const postId = e.target.dataset.id;
+            await updatePost(postId, title, content);
+            formEl.style.display = "none";
+        });
+    }
+});
 // // Обробник події для видалення поста
 // document.addEventListener("click", cb);
 // // Обробник події для додавання коментаря
--- script.js
+++ script.js
@@ -31,6 +31,18 @@ async function createPost(title, content) {
 // Оновлення поста
 async function updatePost(id, title, content) {
   try {
+    const newBody = JSON.stringify(new basePost(title, content));
+    const options = {
+      method: "PATCH",
+      body: newBody,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const r = await fetch(`${BASE_URL}/${id}`, options);
+    const res = await r.json();
+    console.log(res);
+    startApp();
   } catch (error) {
     console.error(error);
   }
@@ -51,13 +63,13 @@ async function createComment(postId, comment) {
 }
 // // Оновлення відображення постів на сторінці
 function renderPosts(posts) {
-    document.getElementById("postsContainer").innerHTML = ''
+  document.getElementById("postsContainer").innerHTML = "";
   const markUp = posts.map((post) => {
     const comments = post.comments;
     return `
             <div class="post">
                 <h2>${post.title}</h2>
-                <p>${post.text}</p>
+                <p>${post.content}</p>
                 <button class="editPostButton" data-id="${
                   post.id
                 }">Редагувати</button>
@@ -69,7 +81,7 @@ function renderPosts(posts) {
                     <ul>
                     ${comments
                       .map((comment) => {
-                        return `<li>${comment.content}</li>`;
+                        return `<li>${comment.text}</li>`;
                       })
                       .join("")}
                     </ul>
@@ -83,6 +95,13 @@ function renderPosts(posts) {
                     <button type="submit">Додати коментар</button>
                     </form>
                 </div>
+              <div class="form-wrap">
+              <form>
+                <input type="text" id="editTitleInput" placeholder="Заголовок" required />
+                <textarea id="editContentInput" placeholder="Зміст" required></textarea>
+                <button type="submit" id="editBtn">Зберегти зміни</button>
+              </form>
+    </div>
             </div>
             `;
   });
@@ -96,9 +115,40 @@ document.getElementById("createPostForm").addEventListener("submit", (e) => {
   const title = document.getElementById("titleInput").value;
   const content = document.getElementById("contentInput").value;
   createPost(title, content);
+  document.getElementById("createPostForm").reset();
 });
 // // Обробник події для редагування поста
-// document.addEventListener("click", cb);
+document.getElementById("postsContainer").addEventListener("click", (e) => {
+  let curId = null;
+  if (e.target.classList.contains("editPostButton")) {
+    e.preventDefault();
+    curId = e.target.dataset.id;
+    const postEl = e.target.closest(".post");
+    const formEl = postEl.querySelector(".form-wrap");
+
+    if (formEl) {
+      formEl.style.display = "block"; 
+      formEl.querySelector('#editTitleInput').value = postEl.querySelector('h2').textContent;
+      formEl.querySelector("#editContentInput").value =
+        postEl.querySelector("p").textContent;
+    }
+
+    const editBtn = postEl.querySelector('#editBtn');
+    if (editBtn) {
+      editBtn.addEventListener('click', async (event) => {
+        event.preventDefault();
+
+        const title = postEl.querySelector('#editTitleInput').value;
+        const content = postEl.querySelector('#editContentInput').value;
+        const postId = e.target.dataset.id;
+
+        await updatePost(postId, title, content);
+        formEl.style.display = "none"; 
+      })
+    }
+  }
+});
+
 // // Обробник події для видалення поста
 // document.addEventListener("click", cb);
 // // Обробник події для додавання коментаря
